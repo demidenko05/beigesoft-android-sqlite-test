1f3c3907f1fddd53d35432868c8b348c8c7afb19
Yury Demidenko <demidenko05@gmail.com>
2020-09-19 15:10:17 +0300

    added proguard to run new bouncycastle1.66 ok

---
+++ src/main/resources/sqlite/init.sql
@@ -0,0 +1,3 @@
+PRAGMA foreign_keys=1;
+PRAGMA encoding='UTF-8'; 
+PRAGMA read_uncommitted=1; 
---
+++ src/main/resources/sqlite/inst.sql
@@ -0,0 +1,4 @@
+insert into USTMC (USR, PWD, VER) values ('admin', 'admin', 1);
+insert into USTMC (USR, PWD, VER) values ('user', 'user', 1);
+insert into USRLTMC (USR, ROL, VER)  values ('admin', 'admin', 1);
+insert into USRLTMC (USR, ROL, VER)  values ('user', 'user', 1);
